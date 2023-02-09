5322ad2ffa3d16c29f6447dce251f42bd1be5d65
David <davidongaroh@gmail.com>
2023-02-09 22:31:44 +0300

    handle messages via loop

--- client.rb
+++ client.rb
@@ -6,3 +6,17 @@ port = 3002
 client = TCPSocket.new(host, port)
 
 puts client.gets.strip
+
+loop do
+    message = gets.strip
+    client.puts message
+  
+    case message
+    when 'EXECUTE'
+      puts client.gets.strip
+    when 'DISCONNECT'
+      break
+    else
+      puts "Unknown command: #{message}"
+    end
+  end
