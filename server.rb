8f1162888691e24823874ea4c7f8f92c47fab4e3
David <davidongaroh@gmail.com>
2023-02-09 22:28:09 +0300

    refactor code

--- server.rb
+++ server.rb
@@ -1,71 +1,71 @@
 require 'socket'
 
 class TcpServer
-    attr_reader :clients, :max_clients
-  
-    def initialize(port, max_clients)
-      @server = TCPServer.new(port)
-      @clients = []
-      @max_clients = max_clients
+  attr_reader :clients, :max_clients
+
+  def initialize(port, max_clients)
+    @server = TCPServer.new(port)
+    @clients = []
+    @max_clients = max_clients
+  end
+
+  def start
+    puts "Server started on port #{@server.addr[1]}"
+
+    loop do
+      client = @server.accept
+
+      if @clients.count < @max_clients
+        puts "Accepted connection from #{client.peeraddr[2]}"
+        client.puts "Welcome! You have been assigned rank #{@clients.count}"
+        @clients << client
+        Thread.new { handle_client(client) }
+      else
+        puts "Rejected connection from #{client.peeraddr[2]}"
+        client.puts "Sorry, the server is full."
+        client.close
+      end
     end
+  end
 
-    def start
-        puts "Server started on port #{@server.addr[1]}"
-    
-        loop do
-          client = @server.accept
-    
-          if @clients.count < @max_clients
-            puts "Accepted connection from #{client.peeraddr[2]}"
-            client.puts "Welcome! You have been assigned rank #{@clients.count}"
-            @clients << client
-            Thread.new { handle_client(client) }
-          else
-            puts "Rejected connection from #{client.peeraddr[2]}"
-            client.puts "Sorry, the server is full."
-            client.close
-          end
-        end
+  def handle_client(client)
+    rank = @clients.index(client)
+
+    loop do
+      message = client.gets.strip
+      command, *params = message.split(' ')
+
+      case command
+      when 'EXECUTE'
+        execute_command(client, rank, params[0].to_i)
+      when 'DISCONNECT'
+        disconnect_client(client, rank)
+        break
+      else
+        puts "Unknown command from client #{rank}: #{message}"
+      end
     end
+  end
 
-        def handle_client(client)
-            rank = @clients.index(client)
+  def execute_command(client, rank, target_rank)
+    if target_rank < rank
+      client.puts "Command executed by client #{rank}"
+      @clients[target_rank].puts "EXECUTED #{rank}"
+    else
+      client.puts "Command rejected by client #{rank}"
+    end
+  end
 
-            loop do
-                message = client.gets.strip
-                command, *params = message.split(' ')
-          
-                case command
-                when 'EXECUTE'
-                  execute_command(client, rank, params[0].to_i)
-                when 'DISCONNECT'
-                  disconnect_client(client, rank)
-                  break
-                else
-                  puts "Unknown command from client #{rank}: #{message}"
-                end
-              end
-        end
+  def disconnect_client(client, rank)
+    puts "Client #{rank} Disconnected!"
+    @clients.delete(client)
+    client.close
 
-        def execute_command(client, rank, target_rank)
-            if target_rank < rank
-              client.puts "Command executed by client #{rank}"
-              @clients[target_rank].puts "EXECUTED #{rank}"
-            else
-              client.puts "Command rejected by client #{rank}"
-            end
-          end
+    @clients.each_with_index do |c, i|
+      c.puts "PROMOTED TO RANK #{i}" if i > rank
+    end
+  end
+end
 
-          def disconnect_client(client, rank)
-            puts "Client #{rank} Disconnected!"
-            @clients.delete(client)
-            client.close
-
-            @clients.each_with_index do |c, i|
-                c.puts "PROMOTED TO RANK #{i}" if i > rank
-              end
-            end
-        end
-      
-        server = TcpServer.new(3002, 5)
-        server.start
+server = TcpServer.new(3000, 3)
+server.start
