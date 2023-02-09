1a6a2d6c6fc97cbcbe8df682e77909c197f3b0dd
David <davidongaroh@gmail.com>
2023-02-09 22:06:25 +0300

    start server & accept limited No. of clients

--- server.rb
+++ server.rb
@@ -6,4 +6,23 @@ class TcpServer
       @clients = []
       @max_clients = max_clients
     end
+
+    def start
+        puts "Server started on port #{@server.addr[1]}"
+    
+        loop do
+          client = @server.accept
+    
+          if @clients.count < @max_clients
+            puts "Accepted connection from #{client.peeraddr[2]}"
+            client.puts "Welcome! You have been assigned rank #{@clients.count}"
+            @clients << client
+            Thread.new { handle_client(client) }
+          else
+            puts "Rejected connection from #{client.peeraddr[2]}"
+            client.puts "Sorry, the server is full."
+            client.close
+          end
+        end
+      end
 end  
